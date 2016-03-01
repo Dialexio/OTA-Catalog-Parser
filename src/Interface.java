064c6e486de316d0c5f60e1bebdb61dd42c16ed8
Alex <Dialexio@users.noreply.github.com>
2016-03-01 13:49:25 -0500

    I knew something would go bad.

--- src/Interface.java
+++ src/Interface.java
@@ -28,7 +28,7 @@ import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
 
 public class Interface {
-	private static final String VERSION = "1.0";
+	private static final String VERSION = "1.0.1";
 
 	private static boolean file = false;
 	private static Display display;
@@ -327,6 +327,6 @@ public class Interface {
 	}
 
 	private static boolean parseButtonStatus() {
-		return file && deviceText.getText().matches("(AppleTV|iPad|iPhone|iPod)(\\d)?\\d,\\d");
+		return file && deviceText.getText().matches("(AppleTV|iPad|iPhone|iPod|Watch)(\\d)?\\d,\\d");
 	}
 }
