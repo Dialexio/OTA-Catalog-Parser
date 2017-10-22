796b2c582d313214f02d85ec270278e1e07465af
Alex <Dialexio@users.noreply.github.com>
2017-10-21 23:27:56 -0400

    Adjusted Mac GUI a bit
    The form elements now stretch with the window if resized. Also updated the custom URL from HTTP to HTTPS (either still works).

--- Octothorpe.Mac/MainWindow.designer.cs
+++ Octothorpe.Mac/MainWindow.designer.cs
@@ -1,7 +1,20 @@
-﻿namespace Octothorpe.Mac
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace Octothorpe.Mac
 {
-	[global::Foundation.Register("MainWindow")]
-	public partial class MainWindow
+	[Register ("MainWindow")]
+	partial class MainWindow
 	{
+		
+		void ReleaseDesignerOutlets ()
+		{
+		}
 	}
 }
