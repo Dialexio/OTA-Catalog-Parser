e1c7ec81f3da77dac3240faf716057f069ce9604
Alex <Dialexio@users.noreply.github.com>
2017-11-03 20:31:43 -0400

    Moved CLI version to Octothorpe.Lib
    Also added the ability to handle integer maximum/minimum values. (i.e. You can specify a minimum of "11" and it will be interpreted as "11.0.")

--- Octothorpe.CLI/Program.cs
+++ Octothorpe.CLI/Program.cs
@@ -20,74 +20,79 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+using Octothorpe.Lib;
 using System;
 using System.IO;
 
 namespace Octothorpe.CLI
 {
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
-			Parser parser = new Parser();
-			int i = 0;
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Parser parser = new Parser();
+            int i = 0;
 
-			while (i < args.Length && args[i][0] == '-')
-			{
-				switch (args[i++])
-				{
-					case "-b":
-						parser.ShowBeta = true;
-						break;
+            while (i < args.Length && args[i][0] == '-')
+            {
+                switch (args[i++])
+                {
+                    case "-b":
+                        parser.ShowBeta = true;
+                        break;
 
-					case "-d":
-						if (i < args.Length)
-							parser.Device = args[i++];
-						break;
+                    case "-d":
+                        if (i < args.Length)
+                            parser.Device = args[i++];
+                        break;
 
-					case "-f":
-						if (i < args.Length)
-							parser.Plist = args[i++];
-						break;
+                    case "-f":
+                        if (i < args.Length)
+                            parser.Plist = args[i++];
+                        break;
 
-					case "-h":
-						Console.WriteLine("OTA Catalog Parser");
-						Console.WriteLine("https://github.com/Dialexio/OTA-Catalog-Parser");
+                    case "-h":
+                        Console.WriteLine("OTA Catalog Parser");
+                        Console.WriteLine("https://github.com/Dialexio/OTA-Catalog-Parser");
 
-						Console.WriteLine("\nRequired Arguments:");
-						Console.WriteLine("-d <device>      Choose the device you are searching for. (e.g. iPhone8,1)");
-						Console.WriteLine("-f <file>        Specify the path to the PLIST file you are searching in.");
-						Console.WriteLine("                 This may be either a local file, or a mesu.apple.com URL.");
-						Console.WriteLine("-m <model>       Choose the model you are searching for. (e.g. N71mAP)\n                 This is only used and required for iPhone 6S or 6S Plus.");
+                        Console.WriteLine("\nRequired Arguments:");
+                        Console.WriteLine("-d <device>      Choose the device you are searching for. (e.g. iPhone8,1)");
+                        Console.WriteLine("-f <file>        Specify the path to the PLIST file you are searching in.");
+                        Console.WriteLine("                 This may be either a local file, or a mesu.apple.com URL.");
+                        Console.WriteLine("-m <model>       Choose the model you are searching for. (e.g. N71mAP)\n                 This is only used and required for iPhone 6S or 6S Plus.");
 
-						Console.WriteLine("\nOptional Arguments:");
-						Console.WriteLine("-b               Displays beta firmwares. By default, this is disabled.");
-						Console.WriteLine("-max <version>   Choose the highest firmware version you are searching for. (e.g. 9.0.2)");
-						Console.WriteLine("-min <version>   Choose the lowest firmware version you are searching for. (e.g. 8.4.1)");
-						Console.WriteLine("-w               Formats the output for The iPhone Wiki.");
-						Environment.Exit(0);
-						break;
+                        Console.WriteLine("\nOptional Arguments:");
+                        Console.WriteLine("-b               Displays beta firmwares. By default, this is disabled.");
+                        Console.WriteLine("-max <version>   Choose the highest firmware version you are searching for. (e.g. 9.0.2)");
+                        Console.WriteLine("-min <version>   Choose the lowest firmware version you are searching for. (e.g. 8.4.1)");
+                        Console.WriteLine("-w               Formats the output for The iPhone Wiki.");
+                        Environment.Exit(0);
+                        break;
 
-					case "-m":
-						if (i < args.Length)
-							parser.Model = args[i++];
-						break;
+                    case "-m":
+                        if (i < args.Length)
+                            parser.Model = args[i++];
+                        break;
 
-					case "-max":
-						if (i < args.Length)
-							parser.Maximum = new Version(args[i++]);
-						break;
+                    case "-max":
+                        if (i < args.Length)
+                            parser.Maximum = (uint.TryParse(args[i++], out var verstring)) ?
+                                new Version(verstring + ".0") :
+                                new Version(verstring.ToString());
+                        break;
 
-					case "-min":
-						if (i < args.Length)
-							parser.Minimum = new Version(args[i++]);
-						break;
+                    case "-min":
+                        if (i < args.Length)
+                            parser.Minimum = (uint.TryParse(args[i++], out var verstring)) ?
+                                new Version(verstring + ".0") :
+                                new Version(verstring.ToString());
+                        break;
 
-					case "-w":
-						parser.WikiMarkup = true;
-						break;
-				}
-			}
+                    case "-w":
+                        parser.WikiMarkup = true;
+                        break;
+                }
+            }
 
             try
             {
@@ -125,11 +130,11 @@ namespace Octothorpe.CLI
                 Console.WriteLine("The program was unable to load the specified file.");
             }
 
-			catch (Exception e)
-			{
+            catch (Exception e)
+            {
                 Console.WriteLine(e.Message);
-				Console.WriteLine(e.StackTrace);
-			}
-		}
-	}
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
 }
